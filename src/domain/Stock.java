a62c505c505f0a5850d61200bc53e57a8369b0af
Lamont <viniciusmessias52@gmail.com>
2025-05-17 08:06:11 -0300

    chore: initial commit

---
+++ src/domain/Stock.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class Stock {
+    private String[] categories = new String[3];
+    private String[] products = new String[6];
+}
---
+++ src/ui/Test1.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
