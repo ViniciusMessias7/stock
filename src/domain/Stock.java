b3948fa00b60fd01bb326363233841ba257d7089
Lamont <viniciusmessias52@gmail.com>
2025-05-17 11:53:50 -0300

    Complete overhaul of Stock.java implementation

--- src/domain/Stock.java
+++ src/domain/Stock.java
@@ -1,6 +1,30 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
-    private String[] categories = new String[3];
-    private String[] products = new String[6];
+    private final List<String> categories;
+    private final List<String> products;
+
+    public Stock() {
+        categories = new ArrayList<>();
+        products = new ArrayList<>();
+    }
+
+    public void addCategories(String category) {
+        categories.add(category);
+    }
+
+    public void addProducts(String product) {
+        products.add(product);
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
 }
